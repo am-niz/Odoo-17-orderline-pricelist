25975f25f456fa638b943cc50d9d085265077e27
nizamudheen <nizamudheenmj@gmail.com>
2024-08-28 13:28:26 +0530

    minor changings

--- wizard/price_list_wizard.py
+++ wizard/price_list_wizard.py
@@ -17,28 +17,31 @@ class PricelistWizard(models.TransientModel):
 
     @api.onchange('pricelist_ids')
     def _onchange_selected(self):
+        """It triggers whenever we select a record on the wizard"""
         selected_line = self.pricelist_ids.filtered('selected')
         if selected_line:
             for line in self.pricelist_ids:
-                if line != selected_line:
-                    line.selected = False
+                if line != selected_line:  # checking the line (record) is not equal to the current selected record
+                    line.selected = False  # uncheck the record
 
     @api.depends('pricelist_ids.selected')
     def _compute_selected_pricelist(self):
+        """It triggers whenever the selected field is change, we can see the above code (line.selected=False)"""
         for wizard in self:
-            selected_line = wizard.pricelist_ids.filtered('selected')
+            selected_line = wizard.pricelist_ids.filtered('selected')  # storing only the selected record
             wizard.selected_pricelist_id = selected_line.pricelist_id if selected_line else False
 
     @api.model
-    def default_get(self, fields_list):  # Renamed from `fields` to `field_names`
+    def default_get(self, fields_list):
         res = super(PricelistWizard, self).default_get(fields_list)
         sale_order_line_id = self.env.context.get('default_sale_order_line_id')
         sale_order_line = self.env['sale.order.line'].browse(sale_order_line_id)
         product_tmpl_id = sale_order_line.product_id.product_tmpl_id.id
 
         pricelist_lines = []
-        pricelists = self.env['product.pricelist'].search([])
+        pricelists = self.env['product.pricelist'].search([])  # For getting all the pricelist that are available
         for pricelist in pricelists:
+            # only taking the pricelist which has the selected product from the sale.order.line
             valid_pricelist_items = self.env['product.pricelist.item'].search([
                 ('pricelist_id', '=', pricelist.id),
                 ('product_tmpl_id', '=', product_tmpl_id),
@@ -49,7 +52,7 @@ class PricelistWizard(models.TransientModel):
                     'name': pricelist.name,
                     'selected': False,
                 }))
-
+        # updating the wizard with new records
         res.update({
             'sale_order_line_id': sale_order_line_id,
             'product_id': sale_order_line.product_id.id,
@@ -77,6 +80,7 @@ class PricelistWizard(models.TransientModel):
             raise UserError("The selected pricelist is not valid for the current conditions.")
 
     def _get_valid_pricelist_items(self, sale_order_line, pricelist):
+        """Filtering the pricelist"""
         domain = [
             ('pricelist_id', '=', pricelist.id),
             ('date_start', '<=', fields.Date.today()),
